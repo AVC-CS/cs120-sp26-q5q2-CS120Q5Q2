dd07c5f80e801e084498dd2c96939cb30e3c38ee
Kyu Woong Lee <kwooler@gmail.com>
2026-03-26 11:12:41 -0700

    Fix: update to string-based regex_test

--- main_test.py
+++ main_test.py
@@ -1,58 +1,41 @@
 import pytest
 import re
 
-def regex_test(expected, lines):
-    i = 0 ; match = 0
+def regex_test(expected, content):
+    pos = 0
     for token in expected:
-        for j in range(i, len(lines)):
-            res = re.search(token, lines[j])
-            if res is not None:
-                i = j + 1
-                match += 1
-                break
-        else:
-            print(f'\033[91m Not Found: {token} \033[0m')
-            assert False, f'Expect: {expected}'
-    else:
-        print(f'\033[91m match count: {match} \033[0m')
-        assert match == len(expected), f'Expect: {expected}'
-
+        res = re.search(token, content[pos:])
+        if res is None:
+            assert False, f'Expect: {token}'
+        pos += res.start() + 1
 
 @pytest.mark.T1
 def test_main_1():
     # N=3, M=3 => 1, 3, 9, 27
-    with open('result1.txt', 'r') as f:
-        lines = f.readlines()
-    print(lines)
-    lines = [line.strip() for line in lines]
-    regex_test([r'1.*3.*9.*27'], lines)
+    content = open('result1.txt').read()
+    print(content)
+    regex_test([r'\b1\b', r'\b3\b', r'\b9\b', r'\b27\b'], content)
 
 
 @pytest.mark.T2
 def test_main_2():
     # N=2, M=4 => 1, 2, 4, 8, 16
-    with open('result2.txt', 'r') as f:
-        lines = f.readlines()
-    print(lines)
-    lines = [line.strip() for line in lines]
-    regex_test([r'1.*2.*4.*8.*16'], lines)
+    content = open('result2.txt').read()
+    print(content)
+    regex_test([r'\b1\b', r'\b2\b', r'\b4\b', r'\b8\b', r'\b16\b'], content)
 
 
 @pytest.mark.T3
 def test_main_3():
     # N=5, M=2 => 1, 5, 25
-    with open('result3.txt', 'r') as f:
-        lines = f.readlines()
-    print(lines)
-    lines = [line.strip() for line in lines]
-    regex_test([r'1.*5.*25'], lines)
+    content = open('result3.txt').read()
+    print(content)
+    regex_test([r'\b1\b', r'\b5\b', r'\b25\b'], content)
 
 
 @pytest.mark.T4
 def test_main_4():
     # N=4, M=3 => 1, 4, 16, 64
-    with open('result4.txt', 'r') as f:
-        lines = f.readlines()
-    print(lines)
-    lines = [line.strip() for line in lines]
-    regex_test([r'1.*4.*16.*64'], lines)
+    content = open('result4.txt').read()
+    print(content)
+    regex_test([r'\b1\b', r'\b4\b', r'\b16\b', r'\b64\b'], content)
